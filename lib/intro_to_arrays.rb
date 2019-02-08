a04775907b1ddda4bfc40f6c4d48094cf2588bc2
Alicia Santiago <albrown07@mac.com>
2019-02-08 20:51:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,22 +21,17 @@ def last_element(my_last_element)
   my_last_element = @taylor_swift[-1]
 end
 
-def first_element(my_first_element)
-  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element = first_element@taylor_swift
-end
-
 def first_element_with_array_methods(first_country)
    @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-   first_country = @south_east_asia
+   first_country = @south_east_asia[0]
 end
 
 def last_element_with_array_methods(last_country)
    @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-   last_country = @south_east_asia
+   last_country = @south_east_asia[-1]
 end
 
 def length_of_an_array(length)
    @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-   length = @programming_languages
+   length = @programming_languages[]
 end
