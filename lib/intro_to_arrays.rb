2a7fdcf566aaa297aba3deac7f587e4593d1b4b1
Alicia Santiago <albrown07@mac.com>
2019-02-08 20:50:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,22 +8,22 @@ end
 
 def first_element(my_first_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element = first_element(@taylor_swift)[0]
+  my_first_element = @taylor_swift[0]
 end
 
 def third_element(my_third_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_third_element = third_element(@taylor_swift)
+  my_third_element = @taylor_swift[2]
 end
 
 def last_element(my_last_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_last_element = last_element(@taylor_swift)
+  my_last_element = @taylor_swift[-1]
 end
 
 def first_element(my_first_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element = first_element(@taylor_swift)
+  my_first_element = first_element@taylor_swift
 end
 
 def first_element_with_array_methods(first_country)
