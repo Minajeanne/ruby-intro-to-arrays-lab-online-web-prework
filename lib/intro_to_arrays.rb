d03c8c24f3936ac9022a66779db97a364ef67704
Alicia Santiago <albrown07@mac.com>
2019-02-08 20:43:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -16,9 +16,9 @@ def third_element(my_third_element)
   my_third_element = third_element(@taylor_swift)
 end
 
-def first_element(my_first_element)
+def last_element(my_last_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element = first_element(@taylor_swift)
+  my_last_element = last_element(@taylor_swift)
 end
 
 def first_element(my_first_element)
